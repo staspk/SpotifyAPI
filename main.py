85e2f17caf18f90f1d7f780d0fbdf91c59b4a608
Stanislav Kozubenko <staspk@gmail.com>
2024-12-24 23:59:42 -0800

    .vscode removed from .gitignore

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ Luna = Spotify(r'Spotify_Data\Luna').getSortedSongStreamingHistory(1800)
 # sharedSongs = Stan.compareSongsStreamed(Luna)
 # Spotify.saveListToFile
 
-Spotify.saveListToFile(Stan, toFile=r'.\Stan_Top_Streamed.txt')
-# Spotify.saveListToFile(Luna, toFile=r'.\Luna_Top_Streamed.txt')
+Spotify.saveListToFile(Stan, toFile=r'.\generated_data\Stan_Top_Streamed.txt')
+# Spotify.saveListToFile(Luna, toFile=r'.\generated_data\Luna_Top_Streamed.txt')
 
 
 print(f'Stan Streamed: {len(Stan)}')
