3f960f8d5104343308c33c83262109ee1234761c
Stanislav Kozubenko <staspk@gmail.com>
2025-01-05 01:08:31 -0800

    Finished Import_Spotify_Data()

--- definitions.py
+++ definitions.py
@@ -6,7 +6,7 @@ PROJECT_ROOT_DIRECTORY          =  os.path.dirname(os.path.abspath(__file__))
 ENV                             =  os.path.join(PROJECT_ROOT_DIRECTORY, '.env', '.env')
 TEMP_DIR                        =  os.path.join(PROJECT_ROOT_DIRECTORY, 'temp')
 SPOTIFY_USER_DATA_DIR           =  os.path.join(PROJECT_ROOT_DIRECTORY, 'Spotify User Data')
-SPOTIFY_ARCHIVED_USER_DATA_DIR  =  os.path.join(SPOTIFY_USER_DATA_DIR, 'Archive')
+SPOTIFY_USER_DATA_ARCHIVE_DIR  =  os.path.join(SPOTIFY_USER_DATA_DIR, 'Archive')
 
 #--- 'my_spotify_data*.zip' Folder Names -----------------------------------------------#
 SPOTIFY_ACCOUNT_DATA                =  'Spotify Account Data'
--- import_spotify_data.py
+++ import_spotify_data.py
@@ -1,66 +1,69 @@
+import os, shutil
 from datetime import datetime
-import os
 from pathlib import Path
-import tkinter as tk
-from tkinter import filedialog
+from tkinter import Tk, filedialog
 
 from kozubenko.os import Downloads_Directory
-from definitions import TEMP_DIR, SPOTIFY_USER_DATA_DIR, SPOTIFY_ACCOUNT_DATA, SPOTIFY_EXTENDED_STREAMING_HISTORY, SPOTIFY_TECHNICAL_LOG, SPOTIFY_ARCHIVED_USER_DATA_DIR
+from definitions import TEMP_DIR, SPOTIFY_USER_DATA_DIR, SPOTIFY_USER_DATA_ARCHIVE_DIR
+from definitions import SPOTIFY_ACCOUNT_DATA, SPOTIFY_EXTENDED_STREAMING_HISTORY, SPOTIFY_TECHNICAL_LOG
 
 from zipfile import ZipFile, ZIP_DEFLATED
 
+# dir_to_archive => full path of directory to archive; DATA_CATEGORY => See 'my_spotify_data*.zip' Folder Names in definitions.py
+def archive(dir_to_archive:str, DATA_CATEGORY):
+    created_at = datetime.fromtimestamp(os.path.getctime(dir_to_archive)).strftime('%Y-%m-%d')
+    name = dir_to_archive.split('\\')[-2]
+    output_target_dir = os.path.join(SPOTIFY_USER_DATA_ARCHIVE_DIR, name)
+    Path(output_target_dir).mkdir(parents=True, exist_ok=True)
 
-def save_user_data_to_project_files(name:str, path_to_zip: str):
-    print(f'path_to_zip: {path_to_zip}')
-    given_zip = ZipFile(path_to_zip)
+    data_category = dir_to_archive.split('\\')[-1]
+    print(f'data_category:{data_category}')
+    output_folder_name = f'{data_category} - {created_at}.zip'                  # example: 'Spotify Extended Streaming History - 2025-01-01'
+    output_target_dir = os.path.join(output_target_dir, output_folder_name)
+    
+    with ZipFile(output_target_dir, 'w', ZIP_DEFLATED) as zip_ref:
+        for cur_dir, folder_names, file_names in os.walk(dir_to_archive):
+            for file_name in file_names:
+                file = os.path.join(cur_dir, file_name)
+                arc_name = os.path.relpath(file, os.path.join(dir_to_archive, '..'))
+                zip_ref.write(file, arc_name)
+    
+    shutil.rmtree(dir_to_archive, ignore_errors=False)      # Deleting folder    
+    print(f'Archive(): dir_to_archive: {dir_to_archive}')
+
+
+def handle_unzip(zipfile_ref:ZipFile, target_folder_in_zip:str, dest_dir:str):
+    if os.path.exists(dest_dir):
+        archive(dest_dir, target_folder_in_zip)
+    
+    for file_name in zipfile_ref.namelist():
+        if file_name.startswith(target_folder_in_zip + '/') and file_name.endswith('.json'):   # Only .jsons to avoid files like readme.pdf
+            zipfile_ref.extract(file_name, os.path.join(dest_dir, '..'))
+    print(f'handle_unzip(): target_folder_in_zip: {target_folder_in_zip}, dest_dir: {dest_dir}')
+
+# name => data owner/username; path_to_zip => path to 'my_spotify_data.zip'
+def save_user_data_to_project_files(name:str, path_to_zip: str):                        # ONLY SPOTIFY EXTENDED STREAMING HISTORY TESTED WITH REAL DATA. PLEASE RUN TESTS WITH CLAUSE 1/3 TOO: 
+    given_zip = ZipFile(path_to_zip, 'r')
     for file_name in given_zip.namelist():
         if file_name == SPOTIFY_ACCOUNT_DATA + '/':
-            zipped_account_data = os.path.join(path_to_zip, SPOTIFY_ACCOUNT_DATA)
-            destination_dir = os.path.join(SPOTIFY_USER_DATA_DIR, name, SPOTIFY_ACCOUNT_DATA)
-            
-            
+            handle_unzip(given_zip, SPOTIFY_ACCOUNT_DATA, os.path.join(SPOTIFY_USER_DATA_DIR, name, SPOTIFY_ACCOUNT_DATA))
 
-        
         if file_name == SPOTIFY_EXTENDED_STREAMING_HISTORY + '/':
-            zipped_account_data = os.path.join(path_to_zip, SPOTIFY_EXTENDED_STREAMING_HISTORY)
-            destination_dir = os.path.join(SPOTIFY_USER_DATA_DIR, name, SPOTIFY_EXTENDED_STREAMING_HISTORY)
-
-            if os.path.exists(destination_dir):     # Save old data under: 'Spotify User Data\archive', as: 'Spotify Extended Streaming History - 2025-01-01'
-                created_at = datetime.fromtimestamp(os.path.getctime(destination_dir)).strftime('%Y-%m-%d')
-                archived_folder_target_dir = os.path.join(SPOTIFY_ARCHIVED_USER_DATA_DIR, name)
-                Path(archived_folder_target_dir).mkdir(parents=True, exist_ok=True)
-
-                archived_folder_name = f'{SPOTIFY_EXTENDED_STREAMING_HISTORY} - {created_at}.zip'
-                archived_folder_target_dir = os.path.join(archived_folder_target_dir, archived_folder_name)
-                
-                with ZipFile(archived_folder_target_dir, 'w', ZIP_DEFLATED) as zip_ref:
-                    for root, folder_names, file_names in os.walk(destination_dir):
-                        for file_name in file_names:
-                            if file_name.endswith('.json'):     # Only want to save the json files holding the data. Goal: avoid the pdf readfirst files.
-                                name = os.path.join(root, file_name)
-                                arc_name = os.path.relpath(name, os.path.join(destination_dir, '..'))
-                                zip_ref.write(name, arc_name)
-
+            handle_unzip(given_zip, SPOTIFY_EXTENDED_STREAMING_HISTORY, os.path.join(SPOTIFY_USER_DATA_DIR, name, SPOTIFY_EXTENDED_STREAMING_HISTORY))
+            
         if file_name == SPOTIFY_TECHNICAL_LOG + '/':
-            temp_dir = os.path.join(TEMP_DIR, 'Spotify Technical Log Information')
+            handle_unzip(given_zip, SPOTIFY_TECHNICAL_LOG, os.path.join(SPOTIFY_USER_DATA_DIR, name, SPOTIFY_TECHNICAL_LOG))
 
     given_zip.close()
 
+def Import_Spotify_Data():
+    name = input('Username/data owner to save this data under?: ')
 
+    root = Tk()
+    root.attributes('-topmost',True, '-alpha',0)
 
-# name = input('What name should we save this file under?: ')
+    path_to_zip = filedialog.askopenfilename(initialdir=Downloads_Directory(), title='Select my_spotify_data*.zip', filetypes=[('zip files', '*.zip')])
 
-# root = tk.Tk()
-# root.attributes('-topmost',True, '-alpha',0)
+    save_user_data_to_project_files(name, path_to_zip)
 
-# path_to_zip = filedialog.askopenfilename(initialdir=Downloads_Directory(), title='Select my_spotify_data*.zip', filetypes=[('zip files', '*.zip')])
-
-name = 'Stan' 
-path_to_zip = 'C:/Users/stasp/Downloads/my_spotify_data.zip'
-
-save_user_data_to_project_files(name, path_to_zip)
-
-# print(f'{name}: {path_to_zip}')
-
-
-
+Import_Spotify_Data()
