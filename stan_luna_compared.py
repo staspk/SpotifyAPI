faaf02fe37a06ef4a93bfaeb92946ac219797fc8
Stanislav Kozubenko <staspk@gmail.com>
2025-03-14 00:39:01 -0700

    comparison complete

---
+++ stan_luna_compared.py
@@ -0,0 +1,23 @@
+import auth_server
+from kozubenko.utils import *
+from spotify_api.spotify_requests import *
+from spotify_stats import SpotifyUser
+from kozubenko.env import Env
+
+from definitions import SPOTIFY_USER_DATA_DIR
+
+
+if __name__ == '__main__':
+    Env.load()
+    auth_server.validate_token()
+    access_token = Env.vars['access_token']
+
+    Stan = SpotifyUser(fr'{SPOTIFY_USER_DATA_DIR}\Stan')
+    Luna = SpotifyUser(fr'{SPOTIFY_USER_DATA_DIR}\Luna')
+
+    shared = Luna.compareStreamedSongs(Stan)
+
+    for song1, song2 in shared:
+        print_cyan(f'{song1.title} - {song1.artist}. Luna: {song1.total_ms_played / 1000 / 60:.0f}mins. Stan: {song2.total_ms_played / 1000 / 60:.0f}mins.\n')
+
+    print(len(shared))
