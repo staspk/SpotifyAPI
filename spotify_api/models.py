0fd55b6a2e58b6e59acb11b34803a3ee90557066
Stanislav Kozubenko <staspk@gmail.com>
2025-03-13 23:09:14 -0700

    making python compliant

--- spotify_api/models.py
+++ spotify_api/models.py
@@ -2,11 +2,10 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class PlaylistId():
-    id:str
-    
+
     def __init__(self, id:str):
-        if id is None:
-            raise ValueError('Id cannot be None')
+        if not id:
+            raise ValueError('Id cannot be Falsy')
         object.__setattr__(self, 'id', id)
 
     def __str__(self):
