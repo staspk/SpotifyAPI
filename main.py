53f3997dcc80af65aac47a60205042a27ad1ad90
Stanislav Kozubenko <staspk@gmail.com>
2025-06-05 20:16:46 -0700

    messing around with some Luna lists

--- main.py
+++ main.py
@@ -13,9 +13,32 @@ if __name__ == '__main__':
     access_token = Env.vars['access_token']
 
     Stan = SpotifyUser(fr'{SPOTIFY_USER_DATA_DIR}\Stan')
+    Luna = SpotifyUser(fr'{SPOTIFY_USER_DATA_DIR}\Luna')
+
+    
+
+    luna = Luna.getSortedSongStreamingHistory(35)
+    luna2 = Luna.getSortedSongStreamingHistory(60)
+    stan = Stan.getLikedSongs()
+
+    luna_minus_stan = [song for song in luna if song not in stan]
+    luna_list       = [song for song in luna if song not in luna2]
+    
+
+    print_list(luna_list)
+    print_red(luna_minus_stan.__len__())
+    print_red(luna_list.__len__())
+
+    # songs = Stan.compareStreamedSongs(Luna)
+    # for stan_song, luna_song in songs:
+    #     print_red(stan_song)
+    #     print_red(luna_song)
+    #     print()
+
 
     response = SaveToPlaylistRequest.New_Playlist(
         access_token,
         'staspk',
-        'Playlist1'
-    ).Handle(list[Song]).Result(True)
+        'Luna Low Grade',
+        description='35mins < listen_time < 60mins'
+    ).Handle(luna_list).Result(True)
