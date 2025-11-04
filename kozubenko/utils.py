8d67d16e98a36e23d0c487bca883445f8cf99e5d
Stanislav Kozubenko <staspk@gmail.com>
2025-11-04 09:00:05 -0800

    +Json.exists()

--- kozubenko/utils.py
+++ kozubenko/utils.py
@@ -1,12 +1,26 @@
-import json, string, random
+import os, json, string, random
+from typing import Any
 
 class Json:
-    def from_file(file:str, encoding='UTF-8'):
+    def load(file:str, encoding='UTF-8'):
         """
         returns `json.load(file)`
         """
         with open(file, 'r', encoding=encoding) as _json:
             return json.load(_json)
+    
+    def exists(path:str, *paths:str, encoding='UTF-8') -> Any | None:
+        """
+        Utility method for opening files, loading json data in one line.
+
+        **Example:**
+        >>> if(data := Json.exists(SPOTIFY_USER_DATA_DIR, self.name, 'Spotify Account Data', 'Userdata.json')):
+                self.account_creation_time = datetime.strptime(data['creationTime'], "%Y-%m-%d")
+        """
+        file = os.path.join(path, *paths)
+        if(os.path.isfile(file)):
+            return Json.load(file, encoding)
+        return None
 
 class Utils:
     def get_randomized_string(str_length):
