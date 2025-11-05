2708b1219ce01a2d755bffad2479c3575d700b69
Stanislav Kozubenko <staspk@gmail.com>
2025-11-04 16:46:56 -0800

    made hash-able, implicit code removed

--- spotify_py/ISong.py
+++ spotify_py/ISong.py
@@ -6,11 +6,9 @@ class ISong():
     title: str
     artist: str
     album: str
-
-    def __eq__(self, other):
-        if not isinstance(other, ISong):
-            return False
-        return (self.title == other.title and self.artist == other.artist and self.album == other.album)
+    
+    def __hash__(self):
+        return hash((self.title, self.artist, self.album))
     
     def __str__(self):
-        return f'{self.title} - {self.artist}'
+        return f'{self.title}:{self.artist}'
