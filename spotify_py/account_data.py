9e0892603465616634b6f3b2dd3d6361b2864705
Stanislav Kozubenko <staspk@gmail.com>
2025-11-04 18:30:08 -0800

    fundamental re-write

--- spotify_py/account_data.py
+++ spotify_py/account_data.py
@@ -1,19 +1,16 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from typing import Any
 from .ISong import ISong
 
 
 @dataclass()
-class LikedSong(ISong):
-    """
-    An instance from `YourLibrary.json['tracks']`  
+class DuplicateSong():
+    song: ISong
+    total_duplicates: int = 1
 
-    `uri` - example form: `spotify:track:4Bxl1qty34HSOKZrRjyj0s`  
-    """
-    uri:str
-
-    def __repr__(self):
-        return f'LikedSong:{self.title}:{self.artist}'
+    def console_report(self):
+        return f'{str(self.song)} -> {self.total_duplicates}'
 
 
 class ILikedSongs():
@@ -22,8 +19,8 @@ class ILikedSongs():
 
     Note: `YourLibrary.json` does not preserve order by time added.
     """
-    songs_liked:       list[LikedSong]
-    songs_duplicates:  dict[LikedSong, int]     # duplicates are found on basis of [song title + artist name] under the hood. See: `ISong`
+    songs_liked:       set[ISong]
+    songs_duplicates:  list[DuplicateSong]
 
 
 class AccountData:
@@ -40,19 +37,17 @@ class AccountData:
             - `AccountData.LikedSongs()` theoretical upper limit: `SpotifyUser.liked.count > 63,234`
         """
         
-        songs_liked:       dict[str, LikedSong] = {}
-        songs_duplicates:  dict[str, int]       = {}
+        liked:       set[ISong]                   = set()
+        duplicates:  dict[ISong, DuplicateSong]   = {}
 
         for record in data['tracks']:
-            song = LikedSong(record['track'], record['artist'], record['album'], record['uri'])
+            song = ISong(record['track'], record['artist'], record['album'])
 
-            fromLiked = songs_liked.get(repr(song), None)
-            if fromLiked is None:
-                songs_liked[repr(song)] = song
+            if(song in liked):
+                duplicate = duplicates.pop(song, DuplicateSong(song))
+                duplicate.total_duplicates += 1
+                duplicates[duplicate.song] = duplicate
             else:
-                numOfDuplicates = songs_duplicates.pop(repr(song), 0)
-                songs_duplicates[repr(song)] = (numOfDuplicates + 1)
+                liked.add(song)                
 
-        return (
-            songs_liked.values(), songs_duplicates
-        )
+        return (liked, duplicates.values())
