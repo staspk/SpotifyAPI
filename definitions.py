8fab30397677c9b978c497d1a1c038c218d1a9e6
Stanislav Kozubenko <staspk@gmail.com>
2025-11-12 18:11:41 -0800

    +Spotify OAuth

--- definitions.py
+++ definitions.py
@@ -1,12 +1,17 @@
 import os, time
 
 
+#--- Spotify Authorization Code Flow  -----------------------------------------------#
+REDIRECT_URI   = 'http://127.0.0.1:8080/callback'
+PERMISSION_SCOPES = 'playlist-read-private playlist-read-collaborative user-library-read user-library-modify playlist-modify-public user-top-read'
+
+
 #--- Absolute Paths to Project Directories ---------------------------------------------#
 PROJECT_ROOT_DIRECTORY          =  os.path.dirname(os.path.abspath(__file__))
 ENV                             =  os.path.join(PROJECT_ROOT_DIRECTORY, '.env', '.env')
 TEMP_DIR                        =  os.path.join(PROJECT_ROOT_DIRECTORY, 'temp')
 SPOTIFY_USER_DATA_DIR           =  os.path.join(PROJECT_ROOT_DIRECTORY, 'Spotify User Data')
-SPOTIFY_USER_DATA_ARCHIVE_DIR   =  os.path.join(SPOTIFY_USER_DATA_DIR, 'Archive')
+SPOTIFY_USER_DATA_ARCHIVE_DIR   =  os.path.join(SPOTIFY_USER_DATA_DIR,  'Spotify User Data', 'Archive')
 
 #--- 'my_spotify_data[x].zip' Folder Names -----------------------------------------------#
 SPOTIFY_ACCOUNT_DATA                =  'Spotify Account Data'
