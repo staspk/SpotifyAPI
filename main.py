af2b69d6d8dfccd61b2770e2ded2e1882831c9c3
Stanislav Kozubenko <staspk@gmail.com>
2025-11-12 18:12:07 -0800

    removed old code

--- main.py
+++ main.py
@@ -1,66 +1,46 @@
+import http
+import os
+from time import sleep
 
-import spotify_py.auth_server as auth_server
+import werkzeug
+from kozubenko.os import File
+from kozubenko.string import String
+from spotify_py.auth_server import AuthServer
+import spotify_py.auth_server2 as auth_server2
 from kozubenko.print import Print
 from spotify_py.spotify_requests import *
 from spotify_py.SpotifyUser import SpotifyUser
 from kozubenko.env import Env
-
-from definitions import SPOTIFY_USER_DATA_DIR
-
-
-STAN = 'Stan'
-LUNA = 'Luna'
-
-stan = SpotifyUser(STAN)
-stan.history.console_report()
-
-luna = SpotifyUser(LUNA)
-luna.history.console_report()
+from definitions import PROJECT_ROOT_DIRECTORY, SPOTIFY_USER_DATA_DIR
 
 
-# .getLostSongCandidates(min_mins_listened=60)
+
+AuthServer().start()
+# AuthServer.get()
+
+time.sleep(12)
+
+
+
+Print.green('now here')
 
 exit(0)
 
-NAME = 'Stan'
-# lost_song_candidates = SpotifyUser(fr'{SPOTIFY_USER_DATA_DIR}{os.path.sep}{NAME}').getLostSongCandidates(min_mins_listened=60)
-
-SaveToPlaylistRequest.New_Playlist(
-	access_token,
-	user_name,
-	playlist_name,
-	description
-).Handle(lost_song_candidates).Result(True)
-
-exit(0)
 
 if __name__ == '__main__':
+    ME = SpotifyUser('Stan')
+    HER = SpotifyUser('Luna')
+
+    her_favorite_songs = SpotifyUser.I_want_her_favorite_songs(ME, HER, minimum_listen_time_in_hours = 3)
+
     Env.load()
-    auth_server.validate_token()
+    auth_server2.validate_token()
     access_token = Env.vars['access_token']
 
-    Stan = SpotifyUser(fr'{SPOTIFY_USER_DATA_DIR}\Stan')
-    Luna = SpotifyUser(fr'{SPOTIFY_USER_DATA_DIR}\Luna')
 
-    
 
-    luna = Luna.getSortedSongStreamingHistory(35)
-    luna2 = Luna.getSortedSongStreamingHistory(60)
-    stan = Stan.getLikedSongs()
 
-    luna_minus_stan = [song for song in luna if song not in stan]
-    luna_list       = [song for song in luna if song not in luna2]
-    
-
-    print_list(luna_list)
-    Print.red(luna_minus_stan.__len__())
-    Print.red(luna_list.__len__())
-
-    # songs = Stan.compareStreamedSongs(Luna)
-    # for stan_song, luna_song in songs:
-    #     Print.red(stan_song)
-    #     Print.red(luna_song)
-    #     print()
+    exit(0)
 
 
     response = SaveToPlaylistRequest.New_Playlist(
